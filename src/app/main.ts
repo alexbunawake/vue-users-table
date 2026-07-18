719fbeeabf470c78fc15a90529263151783f4a15
Aleksei Bunakov <alexman9504@gmail.com>
2026-07-18 11:31:00 +0200

    Integrate `queryClient` with Vue Query Plugin and configure default query/mutation options

--- src/app/main.ts
+++ src/app/main.ts
@@ -3,11 +3,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import { VueQueryPlugin } from '@tanstack/vue-query'
+import { queryClient } from './queryClient'
 import './styles/index.css'
 
 const app = createApp(App)
 
-app.use(VueQueryPlugin)
+app.use(VueQueryPlugin, { queryClient })
 app.use(router)
 
 app.mount('#users-app')
